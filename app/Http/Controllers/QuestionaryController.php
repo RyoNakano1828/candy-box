436d4e6afe141679123cbaa557e55fdf9cb3aaac
ryo.nakano <naca.11868@gmail.com>
2020-08-09 16:23:34 +0900

    回答者の端末情報を記録

--- app/Http/Controllers/QuestionaryController.php
+++ app/Http/Controllers/QuestionaryController.php
@@ -22,9 +22,28 @@ class QuestionaryController extends Controller
         return view('questionary.form');
     }
 
+    /**
+     * クライアントの使用端末がMobileかPCか判定
+     *
+     * @param $request
+     * @return string
+     * @access private
+     */
+    public function isMobileOrPc($request): string
+    {
+        $user_agent =  $request->header('User-Agent');
+        if ((strpos($user_agent, 'iPhone') !== false)
+            || (strpos($user_agent, 'iPod') !== false)
+            || (strpos($user_agent, 'Android') !== false)) {
+            return 'mobile';
+        } else {
+            return 'pc';
+        }
+    }
+
     public function store(Request $req){
 
-        Log::debug($req);
+        Log::debug($req);   
 
         $questionary = new Questionary();
         $questionary->gender = $req->input('gender');
@@ -50,7 +69,9 @@ class QuestionaryController extends Controller
         $questionary->eat_frequency = $req->input('eat_frequency');
         $questionary->sweets_time = $req->input('sweets_time');
         $questionary->family = $req->input('family');
+        $questionary->device = $this->isMobileOrPc($req);
         $questionary->save();
+        //回答者のIDをセッションに追加
         $req->session()->put('questionary_id', $questionary->id);
         return redirect('/candybox');
     }
